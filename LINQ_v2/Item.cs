d7aa45581c669e68a2786bdf8752d56d6c1e0b32
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-03-08 11:31:35 +0200

    Add project files.

---
+++ LINQ_v2/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemClass
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Producer { get; set; }
+        public double Price { get; set; }
+
+        public Item() { }
+        public Item(string name, string producer, double price)
+        {
+            Name = name;
+            Producer = producer;
+            Price = price;
+        }
+        public override string ToString() { return  Name; }
+        public static IEnumerable<Item> GetDifference(Item[] minuend, Item[] subtrahend) 
+        {
+            return minuend.Except(subtrahend);
+        }
+        public static IEnumerable<Item> GetIntersection(Item[] set1, Item[] set2)
+        {
+            return set1.Intersect(set2);
+        }
+        public static IEnumerable<Item> GetUnion(Item[] set1, Item[] set2)
+        {
+            return set1.Union(set2);
+        }
+
+        //public int Compare(Item x, Item y)
+        //{
+        //    return x.Producer.CompareTo(y.Producer);
+        //}
+    }
+}
---
+++ LINQ_v2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ItemClass;    
+
+namespace LINQ_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
